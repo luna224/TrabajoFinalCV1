11978232ba2ef2c3cde4bcfa347cad634971191f
luna224 <126741414+luna224@users.noreply.github.com>
2023-03-02 16:56:02 -0300

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,65 @@
+const button = document.getElementById("boton");
+const link = document.getElementById("link");
+
+link.style.display = "none";
+
+button.addEventListener("click",(event)=>{
+
+    if (link.style.display == "none"){
+        link.style.display = "block"
+    } else {
+        link.style.display = "none"
+    }
+})
+const button2 = document.getElementById("boton2");
+const link2 = document.getElementById("link2");
+
+link2.style.display = "none";
+
+button2.addEventListener("click",(event)=>{
+
+    if (link2.style.display == "none"){
+        link2.style.display = "block"
+    } else {
+        link2.style.display = "none"
+    }
+})
+const button3 = document.getElementById("boton3");
+const link3 = document.getElementById("link3");
+
+link3.style.display = "none";
+
+button3.addEventListener("click",(event)=>{
+
+    if (link3.style.display == "none"){
+        link3.style.display = "block"
+    } else {
+        link3.style.display = "none"
+    }
+})
+const button4 = document.getElementById("boton4");
+const link4 = document.getElementById("link4");
+
+link4.style.display = "none";
+
+button4.addEventListener("click",(event)=>{
+
+    if (link4.style.display == "none"){
+        link4.style.display = "block"
+    } else {
+        link4.style.display = "none"
+    }
+})
+const button5 = document.getElementById("boton5");
+const link5 = document.getElementById("link5");
+
+link5.style.display = "none";
+
+button5.addEventListener("click",(event)=>{
+
+    if (link5.style.display == "none"){
+        link5.style.display = "block"
+    } else {
+        link5.style.display = "none"
+    }
+})
